3dc2c15e024a0d8ffe462b30953bc0258e2dfae4
Melon <v-menwei@microsoft.com>
2017-12-07 11:43:01 +0800

    Initialize blog

--- MelonBlogs/obj/Release/Package/PackageTmp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MelonBlogs.MvcApplication" Language="C#" %>
--- MelonBlogs/obj/Release/Package/PackageTmp/Scripts/myJs/mobile_app.js
+++
@@ -1,8 +0,0 @@
-﻿
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Container = require('./Nav_Touch/Nav_Container').default;
-
-import '../../Content/style.scss';
-
-ReactDOM.render(<Container />, document.getElementById('app'));
